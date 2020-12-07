77cbe33aba4b820a0ef1dc90a6e5f07896fe68b8
Matt Long <matthewhl@gmail.com>
2020-12-07 12:25:35 -0500

    part 2 was kind of a cheat

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() -> std::io::Result<()> {
-    let mut data = File::open("../day1.txt").unwrap();
-    let mut contents = String::new();
-    data.read_to_string(&mut contents).unwrap();
-    let numbers: Vec<usize> = contents.lines().map(|line| line.parse::<usize>().unwrap()).collect();
-    parse_input_part_one(&numbers);
-    parse_input_part_two(&numbers);
-
+    // let mut data = File::open("../day1.txt").unwrap();
+    // let mut contents = String::new();
+    // data.read_to_string(&mut contents).unwrap();
+    // let numbers: Vec<usize> = contents.lines().map(|line| line.parse::<usize>().unwrap()).collect();
+    // parse_input_part_one(&numbers);
+    // parse_input_part_two(&numbers);
+    parse_day_five();
     Ok(())
 }
 
@@ -61,4 +61,59 @@ fn parse_input_part_two(numbers: &Vec<usize>) {
     }
     println!("MATCHING_NUMBER: {} of {}", matching_number, length);
 
+}
+
+fn parse_day_five() {
+    let mut buffer = String::new();
+    let mut file = File::open("./day5.txt").unwrap();
+    file.read_to_string(&mut buffer);
+    let mut seat_id = 0;
+    let mut c = 0;
+    let mut seats: Vec<Option<usize>> = Vec::with_capacity(1024);
+    for _ in 0..1024 {
+        seats.push(None);
+    }
+    for line in buffer.lines() {
+        c += 1;
+        let id = parse_seating(line.chars().collect());
+        if id > seat_id {
+            seat_id = id;
+        }
+
+        seats[id] = Some(id);
+    }
+
+    println!("HIGHEST: {} from {} seats: {:?}", seat_id, c, seats);
+}
+
+fn parse_seating(seating: Vec<char>) -> usize {
+    let mut row_max = 127usize;
+    let mut row_min = 0usize;
+    let mut top_half = (row_max / 2)..row_max;
+    let mut bottom_half = row_min..(row_max / 2);
+    let mut col_max = 7usize;
+    let mut col_min = 0usize;
+    let mut id = 0;
+    for c in seating {
+        match c {
+            'F' => {
+                // take the lower half
+                row_max = (row_max + row_min) / 2;
+            },
+            'B' => {
+                // take the upper half
+                row_min = ((row_max + row_min) / 2) + 1;
+            },
+            'L' => {
+                col_max = (col_max + col_min) / 2;
+            },
+            'R' => {
+                col_min = ((col_max + col_min) / 2) + 1;
+            },
+            _ => panic!("shousdl")
+        }
+    }
+
+    id = row_max * 8 + col_max;
+    id
 }
